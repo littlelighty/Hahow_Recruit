7e7f923a58862e7681dcadf80e5a442919f77a2b
littlelighty <alexchen0855@gmail.com>
2016-12-19 07:54:04 +0800

    Final Adjustments

--- app/app.module.ts
+++ app/app.module.ts
@@ -28,10 +28,3 @@ import { AppRoutingModule }     from './app-routing.module';
   bootstrap: [ AppComponent ]
 })
 export class AppModule { }
-
-
-/*
-Copyright 2016 Google Inc. All Rights Reserved.
-Use of this source code is governed by an MIT-style license that
-can be found in the LICENSE file at http://angular.io/license
-*/
--- app/hero-profile.component.ts
+++ app/hero-profile.component.ts
@@ -175,13 +175,4 @@ export class HeroProfileComponent implements OnInit {
       })
     }
   }
-
-
 }
-
-
-/*
-Copyright 2016 Google Inc. All Rights Reserved.
-Use of this source code is governed by an MIT-style license that
-can be found in the LICENSE file at http://angular.io/license
-*/
